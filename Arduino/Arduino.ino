330ea300606f9d36270e8200aa6644a225011691
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-28 17:06:46 +0200

    Working on advanced pins

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,9 +7,13 @@
 #include "libs/modoc/pins.hpp"
 #include "libs/modoc/fields.hpp"
 
+#include "advanced_pins.hpp"
+#include "pin_setup.hpp"
+
 void setup() {
   // put your setup code here, to run once:
   Pin::Digital x = Pin::Digital::Taste1; 
+  pin_setup();
 }
 
 void loop() {
---
+++ Arduino/advanced_pins.hpp
@@ -0,0 +1,13 @@
+#ifndef ADVANCED_PINS_H
+#define ADVANCED_PINS_H
+
+#include <Servo.h>
+
+struct AdvancedPins {
+
+	Servo* VBox;
+	Servo* EBox;
+
+};
+
+#endif
