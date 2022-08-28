535ba0680c92dc19109ad904c3190e1f89bee061
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-29 01:44:53 +0200

    Update pin_setup.cpp
    Now uses the NeoLED helper

--- Arduino/pin_setup.cpp
+++ Arduino/pin_setup.cpp
@@ -1,14 +1,13 @@
 #include <SPI.h>
 
-// Need to include .cpp files for implementation of class functions/constructor/...
+// Need to include .cpp file for implementation of class functions/constructor/...
 #include "libs/HX711/Q2HX711.cpp"
-#include "libs/AdafruitNeoPixel/Adafruit_NeoPixel.cpp"
 
 #include "libs/modoc/pins.hpp"
 
 #include "advanced_pins.hpp"
+#include "neoled_helper.hpp"
 #include "pin_setup.hpp"
-
 
 AdvancedPins* pin_setup() {
     
@@ -22,23 +21,46 @@ AdvancedPins* pin_setup() {
 	pinMode(Pin::Digital::Taste4, INPUT_PULLUP);
 	pinMode(Pin::Digital::NotAus, INPUT_PULLUP);
 	pinMode(Pin::Digital::OffButton, INPUT_PULLUP);
+	
+	pinMode(Pin::Digital::VCCrelai, OUTPUT);
+	digitalWrite(Pin::Digital::VCCrelai, HIGH);
+		/*
+		Beim Start des Arduino soll der zweite PNP Transistor sperren
+		(das macht er auch ohne HIGH auf diesem Kabel), damit bleibt
+		der erste PNP Transistor leitend und das Relais bleibt angezogen
+		*/
+
+	pinMode(Pin::Digital::RecordLED, OUTPUT);
+	digitalWrite(Pin::Digital::RecordLED, HIGH);
+		/*
+		Beim Starten des Arduino soll der PNP Transistor sperren
+		(-> RecordLED leuchtet nicht)
+		*/
+
+	
 
 	// Analog
 	// Don't need setup using pinMode!
+
+
 
 	// PWM
 	pinMode(Pin::PWM::RcKanal1, INPUT);
 	pinMode(Pin::PWM::RcKanal2, INPUT);
 	pinMode(Pin::PWM::RcKanal3, INPUT);
 
+
+
 	// Advanced pins - Servos
 	AdvancedPins* ap = new AdvancedPins;
 
 	ap->VBox = new Servo;
 	ap->VBox->attach(Pin::PWM::VBox);	
+	ap->VBox->writeMicroseconds(ServoIdle);
 
 	ap->EBox = new Servo;
 	ap->EBox->attach(Pin::PWM::EBox);
+	ap->EBox->writeMicroseconds(ServoIdle);
 
 	// Advanced pins - HX711
 	ap->Drehmoment = new Q2HX711(
@@ -53,10 +75,11 @@ AdvancedPins* pin_setup() {
 
 	// Advanced pins - Neo Pixel LED
 	ap->NeoLED = new Adafruit_NeoPixel(
-		NeoLEDnumPixels, 
+		NeoLED::NumPixels, 
 		Pin::Digital::NeoLED, 
 		NEO_GRB + NEO_KHZ800
 	);
+	NeoLED::begin(ap->NeoLED);
 
 	return ap;
 }
