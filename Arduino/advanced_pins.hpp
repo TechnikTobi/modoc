9795ae6648502fff6bcb92bc422e1c6598ab2639
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-29 00:29:12 +0200

    Added other pointers to AdvancedPins struct

--- Arduino/advanced_pins.hpp
+++ Arduino/advanced_pins.hpp
@@ -3,10 +3,20 @@
 
 #include <Servo.h>
 
+#include "libs/HX711/Q2HX711.h"
+#include "libs/AdafruitNeoPixel/Adafruit_NeoPixel.h"
+
+static const int NeoLEDnumPixels = 3;
+
 struct AdvancedPins {
 
 	Servo* VBox;
 	Servo* EBox;
+
+	Q2HX711* Drehmoment;
+	Q2HX711* Schub;
+
+	Adafruit_NeoPixel* NeoLED;	
 
 };
 
