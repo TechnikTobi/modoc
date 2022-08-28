823566071e61d34139010870107e59099f92179e
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-28 15:21:23 +0200

    Update Arduino.ino
    Include all libraries just to test whether compilation works

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,6 +1,15 @@
+#include <SPI.h>
+#include <Servo.h>
+
+#include "libs/AdafruitNeoPixel/Adafruit_NeoPixel.h"
+#include "libs/HX711/Q2HX711.h"
+
+#include "libs/modoc/pins.hpp"
+#include "libs/modoc/fields.hpp"
+
 void setup() {
   // put your setup code here, to run once:
-
+  Pin::Digital x = Pin::Digital::Taste1; 
 }
 
 void loop() {
