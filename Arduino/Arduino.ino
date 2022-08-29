168dd7c41d77a5c4bb2e8ab7e699c708c35d2401
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-30 00:36:57 +0200

    Update Arduino.ino
    Now contains most of the high level stuff

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,12 +1,10 @@
-#include <SPI.h>
-#include <Servo.h>
-
-#include "libs/modoc/pins.hpp"
-#include "libs/modoc/fields.hpp"
+//#include <SPI.h>
+//#include <Servo.h>
 
 #include "advanced_pins.hpp"
 #include "data.hpp"
 #include "neoled_helper.hpp"
+#include "serial_handler.hpp"
 #include "setup.hpp"
 #include "shutdown.hpp"
 
@@ -16,12 +14,12 @@ static AdvancedPins* advancedPins = NULL;
 
 void setup() {
 
-  Serial.begin(9600);
+  // Initialize serial connection
+  SerialHandler::init();
 
   // Setup & Initialization of all the pins and their devices
   advancedPins = Setup::pin_setup();
   if (advancedPins == NULL) abort();
-
   Setup::pin_init(advancedPins);
 
   // Perform LED startup sequence
@@ -31,8 +29,17 @@ void setup() {
 
 void loop() {
 
+  // Read the data from the pins
   ReadResult* readResult = readData(SOURCE_VERSION);
 
+  // Send the data via the serial connection to the RPi
+  SerialHandler::sendData(
+    readResult->data,
+    readResult->dataSize,
+    advancedPins->NeoLED
+  );
+
+  // Check for shutdown
   if (readResult->shutdown) Shutdown::gracefully(advancedPins->NeoLED);
 
   // Don't forget to free the memory!
