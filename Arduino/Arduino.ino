6ed231dc83452b831b35ecf0eb95910a5f95027e
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-29 16:26:09 +0200

    Update Arduino.ino
    Current status of main program

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,21 +2,26 @@
 #include <Servo.h>
 
 #include "libs/modoc/pins.hpp"
+#include "libs/modoc/fields.hpp"
 
 #include "advanced_pins.hpp"
+#include "data.hpp"
 #include "neoled_helper.hpp"
 #include "setup.hpp"
+#include "shutdown.hpp"
 
-// Allocate memory for the data buffer that holds the different measurements
-// Stored as static const so the pointer persists through the entire operation and can't change
-static const long int *dataBuffer = (long int*) malloc(sizeof(long int));
+#define SOURCE_VERSION 100
+
+static AdvancedPins* advancedPins = NULL;
 
 void setup() {
 
   Serial.begin(9600);
 
   // Setup & Initialization of all the pins and their devices
-  AdvancedPins* advancedPins = Setup::pin_setup();
+  advancedPins = Setup::pin_setup();
+  if (advancedPins == NULL) abort();
+
   Setup::pin_init(advancedPins);
 
   // Perform LED startup sequence
@@ -26,6 +31,12 @@ void setup() {
 
 void loop() {
 
-  
+  ReadResult* readResult = readData(SOURCE_VERSION);
 
+  if (readResult->shutdown) Shutdown::gracefully(advancedPins->NeoLED);
+
+  // Don't forget to free the memory!
+  free(readResult->data);
+  free(readResult);
+  
 }
