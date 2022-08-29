aec2e254fd269a5b517dd3530d9060b0de7eae10
Tobias Prisching <tobias.prisching@icloud.com>
2022-08-29 13:44:41 +0200

    Update fields.hpp
    Divided enum into multiple enums

--- Arduino/libs/modoc/fields.hpp
+++ Arduino/libs/modoc/fields.hpp
@@ -3,64 +3,66 @@
 
 namespace Field {
 
-    enum Index {
+	enum General {			// Indices: 0-19
+		UPM =			0,
+		Drehmoment =		1,
+		Schub =			2,
+		Ausgangsleistung =	3,
+		Strom =			4,
+		Eingangsleistung =	5,
+		Wirkungsgrad =		6,
+		Spritverbrauch =	7,
+		DeltaMax =		8,
+		GasPotiVBox =		9,
+		GasPotiEBox =		10,
+		GasProgramm =		11,
+		Schallpegel =		12,
+		Vibration =		13,
+		RcKanal1 =		14,
+		RcKanal2 =		15,
+		RcKanal3 =		16,
+	};
 
-        // Allgemein: 0-19
-        UPM =			0,
-        Drehmoment =		1,
-        Schub =			2,
-        Ausgangsleistung =	3,
-        Strom =			4,
-        Eingangsleistung =	5,
-        Wirkungsgrad =		6,
-        Spritverbrauch =	7,
-        DeltaMax =		8,
-        GasPotiVBox =		9,
-        GasPotiEBox =		10,
-        GasProgramm =		11,
-        Schallpegel =		12,
-        Vibration =		13,
-        RcKanal1 =		14,
-        RcKanal2 =		15,
-        RcKanal3 =		16,
+	enum Temperatur {		// Indices: 20-29
+		Temperatur1 =		20,
+		Temperatur2 =		21,
+		Temperatur3 =		22,
+		Temperatur4 =		23,
+		Temperatur5 =		24,
+		Temperatur6 =		25,
+		Temperatur7 =		26,
+		Temperatur8 =		27,
+		TemperaturEnv =		28,
+		FeuchtigkeitEnv =	29,
+	};
 
-        // Temperatur & Umgebung: 20-29
-        Temperatur1 =		20,
-        Temperatur2 =		21,
-        Temperatur3 =		22,
-        Temperatur4 =		23,
-        Temperatur5 =		24,
-        Temperatur6 =		25,
-        Temperatur7 =		26,
-        Temperatur8 =		27,
-        TemperaturEnv =		28,
-        FeuchtigkeitEnv =	29,
+	enum Controls {			// Indices: 30-39
+		Taste1 =		30,
+		Taste2 =		31,
+		Taste3 =		32,
+		Taste4 =		33,
+		NotAus =		34,
+		OffButton =		35,
+	};
+	
+	enum Info {			// Indices: 40-49
+		ArduinoVersion =	40,
+		Messdauer =		41,
+		VCCspannung =		42,
+		TemperaturCPU =		43,
+		RTCDate =		44,
+		RTCTime =		45,
+	};
 
-        // MoDoc Controls: 30-39
-        Taste1 =		30,
-        Taste2 =		31,
-        Taste3 =		32,
-        Taste4 =		33,
-        NotAus =		34,
-        OffButton =		35,
+	enum Strings {			// Indices: 50-59
+		Motor =			50,
+		Propeller =		51,
+		Akku =			52,
+		Sprit =			53,
+		Einstellungen =		54,
+		Bemerkungen =		55
 
-        // Info (Arduino & Raspberry): 40-49
-        ArduinoVersion =	40,
-        Messdauer =		41,
-        VCCspannung =		42,
-        TemperaturCPU =		43,
-        RTCDate =		44,
-        RTCTime =		45,
-
-        // Strings: 50-59
-        Motor =			50,
-        Propeller =		51,
-        Akku =			52,
-        Sprit =			53,
-        Einstellungen =		54,
-        Bemerkungen =		55
-
-    };
+	};
 };
 
 #endif
